cf3096ea1bd0c57c293efa828aa9d04174efd609
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-11-14 11:50:12 +0100

    Added handeling for enabeling and disabeling the task plugin.

--- src/demeter_task.c
+++ src/demeter_task.c
@@ -4,6 +4,7 @@
 // Wow, such plugin, much data!
 //___________________________________________________________________________________________________________________________________________
 
+#include <stdbool.h>
 #include "src/common/log.h"
 #include "src/slurmd/common/task_plugin.h"
 #include "demeter_task.h"
@@ -17,34 +18,38 @@ static demeter_conf_t *demeter_conf = NULL;
 
 extern int init (void)
 {
-	demeter_conf = read_conf();
-	if (demeter_conf == NULL)
-		return SLURM_ERROR;
-	debug("%s loaded", plugin_name);
-	return SLURM_SUCCESS;
+    demeter_conf = read_conf();
+    if (demeter_conf == NULL)
+        return SLURM_ERROR;
+    debug("%s loaded", plugin_name);
+    return SLURM_SUCCESS;
 }
 
 extern int fini (void)
 {
-	free_cgroup(cgroup_data);
-	free_job_id_info(task_info);
-	free_conf(demeter_conf);
+    free_cgroup(cgroup_data);
+    free_job_id_info(task_info);
+    free_conf(demeter_conf);
     return SLURM_SUCCESS;
 }
 
 extern int task_p_post_term (stepd_step_rec_t *job, stepd_step_task_info_t *task)
 {
-	task_info = get_task_info(job);
-	if (task_info == NULL)
-		return SLURM_ERROR;
-	cgroup_data = gather_cgroup(task_info, demeter_conf);
-	if (cgroup_data == NULL)
-		return SLURM_ERROR;
-	// log_cgroup(cgroup_data, task_info, demeter_conf);
-	transfer_log_cgroup(cgroup_data, task_info, demeter_conf);
-	free_cgroup(cgroup_data);
-	free_job_id_info(task_info);
-	return SLURM_SUCCESS;
+    if ( !demeter_conf || (demeter_conf && !demeter_conf->using_task_plugin)) {
+        free_job_id_info(task_info);
+        write_log_to_file(demeter_conf,"task plugin not used", INFO, 0);
+        return SLURM_SUCCESS;
+    }
+    task_info = get_task_info(job);
+    if (task_info == NULL)
+        return SLURM_ERROR;
+    cgroup_data = gather_cgroup(task_info, demeter_conf);
+    if (cgroup_data == NULL)
+        return SLURM_ERROR;
+    transfer_log_cgroup(cgroup_data, task_info, demeter_conf);
+    free_cgroup(cgroup_data);
+    free_job_id_info(task_info);
+    return SLURM_SUCCESS;
 }
 
 // UNUSED
@@ -52,55 +57,55 @@ extern int task_p_post_term (stepd_step_rec_t *job, stepd_step_task_info_t *task
 
 extern int task_p_slurmd_batch_request (batch_job_launch_msg_t *req)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_slurmd_launch_request (launch_tasks_request_msg_t *req, uint32_t node_id)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_slurmd_suspend_job (uint32_t job_id)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_slurmd_resume_job (uint32_t job_id)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_pre_setuid (stepd_step_rec_t *job)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_pre_launch (stepd_step_rec_t *job)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_pre_set_affinity(stepd_step_rec_t *job, uint32_t node_tid)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_set_affinity(stepd_step_rec_t *job, uint32_t node_tid)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_post_set_affinity(stepd_step_rec_t *job, uint32_t node_tid)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_post_step (stepd_step_rec_t *job)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
 
 extern int task_p_add_pid (pid_t pid)
 {
-	return SLURM_SUCCESS;
+    return SLURM_SUCCESS;
 }
